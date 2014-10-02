16ad146ed9575eea50bdec6ec01ab3d1ca45617a
Philip Nguyen <supertaru@gmail.com>
2014-10-02 09:49:48 -0700

    Fix rubocop crap

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   root 'works#index'
 
   get 'my_desk' => 'works#my_desk'
-  # The priority is based upon order of creation: first created -> highest priority.
+  # The priority is based upon order of creation: first created -> highest
+  # priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
@@ -15,10 +16,12 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
+  # Example of named route that can be invoked with
+  # purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions
+  # automatically):
   #   resources :products
 
   # Example resource route with options:
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,33 +10,36 @@ require 'minitest/rails/capybara'
 # Uncomment for awesome colorful output
 require 'minitest/pride'
 
-class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+module ActiveSupport
+  class TestCase
+    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
+    # order.
+    fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  def sign_in(test_user = :user_1)
-    visit new_user_session_path
-    fill_in 'Email', with: users(test_user).email
-    fill_in 'Password', with: 'password'
-    click_on 'Log in'
-  end
+    # Add more helper methods to be used by all tests here...
+    def sign_in(test_user = :user_1)
+      visit new_user_session_path
+      fill_in 'Email', with: users(test_user).email
+      fill_in 'Password', with: 'password'
+      click_on 'Log in'
+    end
 
-  def sign_out
-    click_on 'Sign out'
-  end
+    def sign_out
+      click_on 'Sign out'
+    end
 
-  def create_work(title = 'Auto')
-    click_on 'New work'
-    fill_in 'Title', with: title
-    fill_in 'Body', with: 'The body of the test work'
-    click_on 'Create Work'
-  end
+    def create_work(title = 'Auto')
+      click_on 'New work'
+      fill_in 'Title', with: title
+      fill_in 'Body', with: 'The body of the test work'
+      click_on 'Create Work'
+    end
 
-  def edit_work(id)
-    visit edit_work_path id
-    fill_in 'Body', with: 'Trolled!'
-    click_on 'Update Work'
+    def edit_work(id)
+      visit edit_work_path id
+      fill_in 'Body', with: 'Trolled!'
+      click_on 'Update Work'
+    end
   end
 end
 
